4e45f294d3531f9fb389e6231b1dc8d7005d19c2
patrickatcw <pj8392@yahoo.com>
2018-02-16 17:40:00 -0500

    fields completed, work on parameters in constructor next

--- src/com/me/Alien.java
+++ src/com/me/Alien.java
@@ -2,6 +2,17 @@ package com.me;
 
 public class Alien {
 
+    //Step 1 create fields for Alien
+    private String from;
+    private String color;
+    private String height;
+    private String body;
+    private String clothes;
 
+    //Step 2 make my own constructor method with parameters
+    public Alien() {
+
+
+    }
 
 }
